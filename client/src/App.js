db2f237943a2cf6e521099191a3c6a3fe36447bd
mrjunoh <mrjunoh@naver.com>
2023-02-27 15:58:44 +0900

    23.02.27

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import parse from 'html-react-parser';
+import axios from 'axios';
 
 function App() {
   const [movieContent, setMovieContent] = useState({
@@ -12,6 +13,32 @@ function App() {
   })
 
   const [viewContent, setViewContent] = useState([]);//입력 누르면 내용 업데이트 state
+
+  //useeffect hook을 사용하는데 2번째 인자에 []빈 배열을 넣어주면
+  // 이 코드를 처음 1회만 실행한다는 의미이다.
+  //아까 서버쪽에 작성한 코드로 요청을 보내고 그에 대한 
+  //응답을 콘솔로 찍으면 아래 사진처럼 data에 내용을 받아오는 것을 확인할 수 있다.
+  useEffect(()=>{
+    axios.get('http://localhost:8000/api/get').then((response)=>{
+      console.log(response.data);
+    })
+  },[viewContent])//[viewContent] 이걸 해줌으로써 업데이트된 모습을 바로 확인가능하다.
+//   이제 이렇게 받아온 데이터를 아까전에 만들어놓은 빈 배열 state인 viewContent 에 담아줄 것이다.
+// response.data 를 viewCount 에 업데이트 해주고 나면 아래와 같이 
+//db의 내용이 화면에 잘 출력되는 것을 볼 수 있다!
+
+
+  //눌렸을 때 서버로 내용을 전송하는 함수를 만들어줌
+  const submitReview = ()=>{
+    //axios를 이용해 title, content 데이터를 post방식으로 보내고 완료되면 alert 창을 띄워주는 코ㅡㄷ
+    // /api/insert는 우리가 localhost:8000/api/insert로 내용을 전송한다는 뜻
+    axios.post('http://localhost:8000/api/insert', {
+      title: movieContent.title,
+      content: movieContent.content
+    }).then(()=>{
+      alert('등록 완료!');
+    })
+  };
 
 
   //input의 내용이 변할 때 그값을 state에 업데이틑 해주는 기능 넣어야한다.
@@ -74,9 +101,7 @@ function App() {
         />
       </div>
       <button className="submit-button"
-              onClick={()=>{
-                setViewContent(viewContent.concat({...movieContent}));
-              }}
+              onClick={submitReview}
               //입력 버튼 클릭스 위 setState배열에 추가하는 기능
               //버튼을 눌렀을 때 viewContent라는 배열 안에 movieContent라는 객체를 복사해서 
               //concat 해준다음 그 내용으로 변경 해주는 내용이다.
---
+++ server/server.js
@@ -0,0 +1,39 @@
+const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const port = 8000
+const mysql = require('mysql');
+const {urlencoded} = require('body-parser');
+const cors = require('cors');
+
+const db = mysql.createPool({
+  host:"localhost",
+  user:"root",
+  password:"비번",
+  database:"simpleboard"
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/api/get', (req, res)=>{
+  const sql = "SELECT * FROM board;";
+  db.query(sql, (err, result)=>{
+    res.send(result);
+  })
+})
+
+app.post('/api/insert', (req, res) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  const sqlQuery = "INSERT INTO board (title, content) VALUES (?,?)";
+  db.query(sqlQuery, [title, content], (err, result) => {
+      res.send('success!');
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
